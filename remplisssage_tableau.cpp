435e846f66c10a225fb6d7d1cec2ceebf005ed19
Khadidjasyh <136167526+Khadidjasyh@users.noreply.github.com>
2023-12-28 09:45:59 +0100

    Add files via upload

---
+++ remplisssage_tableau.cpp
@@ -0,0 +1,13 @@
+	//initialisation de la taille du tableau
+    	int main(){
+		
+		while((n<0)||n>100){
+    		printf("veuillez entrez la taille du tableau");
+    		scanf("%d",&n);
+		}
+		//remplissage
+		for (i=0;i<n:i++){
+		printf("donnez la valeur de arr[%d]:",i+1);
+		scanf("%d",&arr[i]);
+		}
+	}
